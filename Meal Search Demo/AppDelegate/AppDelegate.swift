58a3b7460c0ca2850f0324f6880b47cad97594a8
IrfanAjmeri <irfan.a@digitral.com>
2024-07-19 17:39:51 +0530

    Added fixes for Dark Mode

--- Meal Search Demo/AppDelegate/AppDelegate.swift
+++ Meal Search Demo/AppDelegate/AppDelegate.swift
@@ -23,7 +23,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         let mealCategoriesViewModel = MealCategoriesViewModel(useCaseProvider: MealRepo())
         let mealCategoriesController = MealCategoriesController(viewModel: mealCategoriesViewModel)
-        window?.rootViewController = UINavigationController(rootViewController: mealCategoriesController)
+        let rootViewController = UINavigationController(rootViewController: mealCategoriesController)
+        let navigationController = setupNavigationBar(rootViewController)
+        window?.rootViewController = navigationController
         return true
+    }
+    
+    func setupNavigationBar(_ navigationController: UINavigationController) -> UINavigationController {
+        UIBarButtonItem.appearance().tintColor = .systemBlue // update here UIBarButtonItem tint color
+        // NAVIGATION BAR CUSTOMIZATION
+        navigationController.navigationBar.prefersLargeTitles = false
+        navigationController.navigationBar.tintColor = UIColor.white
+        navigationController.navigationBar.isTranslucent = false
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithDefaultBackground()
+            appearance.backgroundColor = UIColor.white
+            appearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+            navigationController.navigationBar.compactAppearance = appearance
+        } else {
+            navigationController.navigationBar.barTintColor = UIColor.white
+            navigationController.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            navigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        }
+        // NAVIGATION BAR SHADOW
+        navigationController.navigationBar.layer.masksToBounds = false
+        navigationController.navigationBar.layer.shadowColor = UIColor.lightGray.cgColor
+        navigationController.navigationBar.layer.shadowOffset = CGSize(width: 0, height: 0.5)
+        navigationController.navigationBar.layer.shadowRadius = 2
+        navigationController.navigationBar.layer.shadowOpacity = 0.5
+        //
+        return navigationController
     }
 }
